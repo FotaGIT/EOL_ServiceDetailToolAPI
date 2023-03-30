5f69303be1a0c43c7b42bcfe1b0e867452980556
Ishwar <ishwara.ttl@tatamotors.com>
2023-03-30 10:04:10 +0530

    initial setup with added components

--- project/settings.py
+++ project/settings.py
@@ -12,21 +12,24 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gylvd*#(zqst&1mwt9(-arc9m8*a01cat%*k*k7xo*@mnr7a$k'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # Application definition
 
@@ -71,17 +74,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': env('CERTIFICATE_DB_NAME'),
+        'USER': env('CERTIFICATE_DB_USER'),
+        'PASSWORD': env('CERTIFICATE_DB_PASSWORD'),
+        'HOST': env('CERTIFICATE_DB_HOST'),
+        'PORT': env('CERTIFICATE_DB_PORT'),
+        'OPTIONS': {
+            'DRIVER': env('CERTIFICATE_DB_DRIVER'),
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -101,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -113,7 +120,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -123,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'service_tool_api.User'
--- service_tool_api/models.py
+++ service_tool_api/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    def save(self, *args, **kwargs):
+        return super().save(*args, **kwargs)
+
+
+class CvpHuNonprodCa(models.Model):
+    created_on                  = models.DateTimeField()
+    modified_on                 = models.DateTimeField(blank=True, null=True)
+    certificate_creation_date   = models.DateTimeField(blank=True, null=True)
+    certificate_expiration_date = models.DateTimeField(blank=True, null=True)
+    certificate_revocation_date = models.DateTimeField(blank=True, null=True)
+    certificate_serial_number   = models.CharField(primary_key=True, max_length=60)
+    certificate                 = models.TextField(blank=True, null=True)
+    private_key                 = models.TextField(blank=True, null=True)
+    common_name                 = models.CharField(max_length=50, blank=True, null=True)
+    is_revoked                  = models.CharField(max_length=1)
+
+    class Meta:
+        managed = False
+        db_table = 'CVP_HU_NONPROD_CA'
+
+
+class CvpTcuNonprodPvevCa(models.Model):
+    created_on                  = models.DateTimeField()
+    modified_on                 = models.DateTimeField(blank=True, null=True)
+    certificate_creation_date   = models.DateTimeField(blank=True, null=True)
+    certificate_expiration_date = models.DateTimeField(blank=True, null=True)
+    certificate_revocation_date = models.DateTimeField(blank=True, null=True)
+    certificate_serial_number   = models.CharField(primary_key=True, max_length=60)
+    certificate                 = models.TextField(blank=True, null=True)
+    private_key                 = models.TextField(blank=True, null=True)
+    common_name                 = models.CharField(max_length=50, blank=True, null=True)
+    is_revoked                  = models.CharField(max_length=1)
+
+    class Meta:
+        managed = False
+        db_table = 'CVP_TCU_NONPROD_PVEV_CA'
+
